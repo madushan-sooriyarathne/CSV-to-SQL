bb1dc06ef7b9695681d8b53e2f45652f9b68074e
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2019-05-09 16:52:53 +0530

    Update and rename __init__.py to main.py

--- src/main.py
+++ src/main.py
@@ -18,7 +18,6 @@ def main():
 
     figlet = Figlet()
     print(figlet.renderText("CSV TO SQL"))
-    print("by Madushan Sooriyarathne")
     args = docopt(USAGE)
     
     if args['<csv_file_path>'] is None or args['<database_file_path>'] is None:
@@ -26,21 +25,6 @@ def main():
     else:
         converter_obj = csvconverter.CSVConverter(args['<csv_file_path>'], args['<database_file_path>'])
         converter_obj.convert()
-
-
-    # Printing the program intro :) 
-    # ? Just to be cool
-    
-
-    # # Getting the input from user (csv file name & db file name)
-    # csv_file_name = input("Enter the name of csv file : ")
-    # db_name = input("Enter the db file name : ")
-    # table_name =csv_file_name.replace('.csv', '').replace('-', '')
-
-    # if len(csv_file_name) == 0 or len(db_name) == 0:
-    #     print(f"{'csv file name' if len(csv_file_name) else 'database file name'} can't be empty ")
-    #     return None
-
     
 if __name__ == "__main__":
     main()
